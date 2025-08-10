98a96d4ff6ddbc37f50228e01d5e88b2e2ca3c89
JespK77 <jesperk777@gmail.com>
2025-08-10 15:43:50 +0200

    tools can now have a custom widget

--- Source/BuildToolSystem/UI/ToolPropertiesWidget.cpp
+++ Source/BuildToolSystem/UI/ToolPropertiesWidget.cpp
@@ -3,8 +3,10 @@
 #include "Components/PanelWidget.h"
 #include "Widgets/Input/SSlider.h"
 #include "Widgets/Layout/SUniformWrapPanel.h"
+#if WITH_EDITORONLY_DATA
+#include "WidgetUtilities/Widgets/DynamicEditorWidget.h"
+#endif
 
-#include "BlueprintUtilities/BlueprintFunctionLibrary/ClassUtilities.h"
 #include "../Game/BuildToolComponent.h"
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -13,6 +15,7 @@ void UToolPropertiesWidgetBase::NativeConstruct() {
 	Super::NativeConstruct();
 	ToolComponent = GetOwningPlayer()->GetComponentByClass<UBuildToolComponent>();
 	if (ToolComponent) ToolComponent->OnToolChanged.AddDynamic(this, &UToolPropertiesWidgetBase::OnActiveToolChanged);
+	else UE_LOG(LogTemp, Warning, TEXT("No ToolComponent found on owned player"));
 }
 
 void UToolPropertiesWidgetBase::NativeDestruct() {
@@ -21,6 +24,16 @@ void UToolPropertiesWidgetBase::NativeDestruct() {
 	ToolComponent = nullptr;
 }
 
-void UToolPropertiesWidgetBase::OnActiveToolChanged(int32 index) {
-	SetObject(ToolComponent->GetActiveTool());
+void UToolPropertiesWidgetBase::OnActiveToolChanged_Implementation(int32 index) {
+	UBuildTool* tool = ToolComponent->GetActiveTool();
+	UUserWidget* newWidget;
+
+	if (IsValid(tool) && tool->ToolWidget) newWidget = CreateWidget(this, tool->ToolWidget, "ToolWidget");
+	else newWidget = nullptr;
+
+	if (newWidget != ActiveWidget) {
+		if (ActiveWidget) ActiveWidget->Destruct();
+		ActiveWidget = newWidget;
+		SetPropertyWidget(ActiveWidget);
+	}
 }
--- Source/BuildToolSystem/UI/ToolPropertiesWidget.h
+++ Source/BuildToolSystem/UI/ToolPropertiesWidget.h
@@ -1,21 +1,31 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "WidgetUtilities/Widgets/DynamicEditorWidget.h"
+#include "CommonUserWidget.h"
 #include "ToolPropertiesWidget.generated.h"
 
 class UBuildTool;
 class UBuildToolComponent;
+#if WITH_EDITORONLY_DATA
+class UDynamicEditorWidget;
+#endif
 
 UCLASS(Abstract)
-class BUILDTOOLSYSTEM_API UToolPropertiesWidgetBase : public UDynamicEditorWidget {
+class BUILDTOOLSYSTEM_API UToolPropertiesWidgetBase : public UCommonUserWidget {
 	GENERATED_BODY()
 
 protected:
 	UPROPERTY(Category = "References", BlueprintReadOnly)
 	TObjectPtr<UBuildToolComponent> ToolComponent;
+	UPROPERTY(Category = "References", BlueprintReadOnly)
+	TObjectPtr<UUserWidget> ActiveWidget;
 
-	UFUNCTION() void OnActiveToolChanged(int32 index);
+	UFUNCTION(Category = "Property Widget", BlueprintImplementableEvent)
+	void SetPropertyWidget(UUserWidget* widget);
+
+	UFUNCTION(Category = "Events", BlueprintNativeEvent)
+	void OnActiveToolChanged(int32 index);
+	virtual void OnActiveToolChanged_Implementation(int32 index);
 
 	virtual void NativeConstruct() override;
 	virtual void NativeDestruct() override;
